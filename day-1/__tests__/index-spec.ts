7166c14633a3831455890b024729391191bfd79d
Chris Campbell <chris@harto.org>
2018-12-04 22:17:58 +1100

    Day 1 - part 2

--- day-1/__tests__/index-spec.ts
+++ day-1/__tests__/index-spec.ts
@@ -1,5 +1,16 @@
-import * as index from '../src/index';
+import * as index from "../src/index";
 
-test('Should have Greeter available', () => {
-  expect(index.Greeter).toBeTruthy();
+describe("Day 2", () => {
+  test("Examples", () => {
+    const examples = [
+      [[1, -1], 0],
+      [[3, 3, 4, -2, -4], 10],
+      [[-6, +3, +8, +5, -6], 5],
+      [[+7, +7, -2, -7, -4], 14]
+    ];
+
+    examples.forEach(([input, output]) => {
+      expect(index.findFirstDuplicate(input as number[])).toEqual(output);
+    });
+  });
 });
--- day-1/src/index.ts
+++ day-1/src/index.ts
@@ -1,12 +1,40 @@
 import { readFileSync } from "fs";
 
-const input = readFileSync("input", { encoding: "utf-8" });
+export function frequencies() {
+  const input = readFileSync("input", { encoding: "utf-8" });
+  return input
+    .trim()
+    .split("\n")
+    .map(f => parseInt(f, 0));
+}
 
-const output = input
-  .trim()
-  .split("\n")
-  .reduce((frequency, next) => {
-    return frequency + parseInt(next, 0);
-  }, 0);
+// Day 1.
+export function calibrateFrequency(input: number[]) {
+  return input.reduce((frequency, next) => frequency + next, 0);
+}
 
-console.log("Frequency: %d", output);
+// Day 2.
+export function findFirstDuplicate(input: number[]) {
+  const values = [0];
+  let output: number | undefined;
+
+  while (output === undefined) {
+    for (const frequency of input) {
+      const prev = values[values.length - 1];
+      const value = prev + frequency;
+
+      if (values.indexOf(value) !== -1) {
+        output = value;
+        break;
+      }
+
+      values.push(value);
+    }
+  }
+
+  return output;
+}
+
+const input = frequencies();
+console.log("Day 1 – Part 1: %d", calibrateFrequency(input));
+console.log("Day 1 - Part 2: %d", findFirstDuplicate(input));
