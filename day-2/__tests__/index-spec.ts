f37f6b1783a51ead3d719e039bb7d39304931ecd
Chris Campbell <chris@harto.org>
2018-12-04 23:23:19 +1100

    Day 2: Part 1

--- day-2/__tests__/index-spec.ts
+++ day-2/__tests__/index-spec.ts
@@ -1,5 +1,17 @@
 import * as index from "../src/index";
 
 describe("Day 2", () => {
-  test("Examples", () => {});
+  test("Checksum", () => {
+    const input = [
+      "abcdef",
+      "bababc",
+      "abbcde",
+      "abcccd",
+      "aabcdd",
+      "abcdee",
+      "ababab"
+    ];
+
+    expect(index.checksum(input)).toEqual(12);
+  });
 });
