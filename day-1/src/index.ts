69dc8c26661380cc251d8619be9d2f8beb5db95b
Chris Campbell <chris@harto.org>
2018-12-04 21:17:32 +1100

    Day 1 solution

--- day-1/src/index.ts
+++ day-1/src/index.ts
@@ -1,1 +1,12 @@
-export * from './greeter';
+import { readFileSync } from "fs";
+
+const input = readFileSync("input", { encoding: "utf-8" });
+
+const output = input
+  .trim()
+  .split("\n")
+  .reduce((frequency, next) => {
+    return frequency + parseInt(next, 0);
+  }, 0);
+
+console.log("Frequency: %d", output);
