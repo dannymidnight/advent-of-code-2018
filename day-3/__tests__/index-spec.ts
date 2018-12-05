e9632c01b3b4e2bb4bb9a4e74ca890ec845908d0
Chris Campbell <chris@harto.org>
2018-12-06 09:53:13 +1100

    Day 3: Part 1

--- day-3/__tests__/index-spec.ts
+++ day-3/__tests__/index-spec.ts
@@ -1,1 +1,22 @@
-describe("Day 3", () => {});
+import { createMap, findOverlaps } from "../src/index";
+
+describe("Day 3", () => {
+  test("Draw fabric map", () => {
+    const input = ["#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2"];
+    const expected = [
+      "........",
+      "...2222.",
+      "...2222.",
+      ".11XX22.",
+      ".11XX22.",
+      ".111133.",
+      ".111133.",
+      "........"
+    ].join("\n");
+
+    const output = createMap(8, input);
+
+    expect(output).toEqual(expected);
+    expect(findOverlaps(output)).toEqual(4);
+  });
+});
--- day-3/src/index.ts
+++ day-3/src/index.ts
@@ -5,3 +5,53 @@ function input() {
     .trim()
     .split("\n");
 }
+
+function toString(map: string[][]): string {
+  const out = [];
+  for (const row of map) {
+    out.push(row.join(""));
+  }
+  return out.join("\n");
+}
+
+export function findOverlaps(map: string) {
+  const count = map.split("").filter(c => c === "X").length;
+  return count;
+}
+
+export function createMap(size: number, input: string[]) {
+  let map: string[][] = [];
+
+  // Initialise map
+  for (let i = 0; i < size; i++) {
+    map[i] = [];
+    for (let j = 0; j < size; j++) {
+      map[i][j] = ".";
+    }
+  }
+
+  for (const raw of input) {
+    const [, id, xRaw, yRaw, width, height] = raw.match(
+      /#(\d+) @ (\d+),(\d+): (\d+)x(\d+)/
+    )!;
+    const x = parseInt(xRaw, 0);
+    const y = parseInt(yRaw, 0);
+
+    for (let col = x; col < x + parseInt(width, 0); col++) {
+      for (let row = y; row < y + parseInt(height, 0); row++) {
+        const current = map[row][col];
+
+        if (current === ".") {
+          map[row][col] = id;
+        } else {
+          map[row][col] = "X";
+        }
+      }
+    }
+  }
+
+  return toString(map);
+}
+
+const overlaps = findOverlaps(createMap(1000, input()));
+console.log(`Day 3 - Part 1: ${overlaps}`);
