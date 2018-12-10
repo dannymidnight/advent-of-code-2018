3290d1d72809ed8685fcce4cea957aa78e7acb01
Chris Campbell <chris@harto.org>
2018-12-10 14:22:42 +1100

    Day 4: Part 1

---
+++ day-4/src/findMostMinutes.ts
@@ -0,0 +1,124 @@
+import { flatten } from "lodash";
+
+enum EventType {
+  Start = "Start",
+  Sleep = "Sleep",
+  Wake = "Wake"
+}
+
+interface Record {
+  date: string;
+  mins: number[];
+  id: number;
+}
+
+interface Event {
+  date: string;
+  type: EventType;
+  id: number;
+}
+
+export function transform(input: string[]): Record[] {
+  let events = input.map(i => {
+    const [, dateRaw, actionRaw] = i.match(/\[(.*)\] (.*)/)!;
+
+    let event: EventType;
+    let id = 0;
+
+    if (actionRaw === "wakes up") {
+      event = EventType.Wake;
+    } else if (actionRaw === "falls asleep") {
+      event = EventType.Sleep;
+    } else {
+      const [, idRaw] = actionRaw.match(/#(\d+)/)!;
+      id = parseInt(idRaw, 0);
+      event = EventType.Start;
+    }
+
+    return {
+      date: dateRaw,
+      id,
+      type: event
+    };
+  });
+
+  events = events.sort((a, b) => {
+    if (a.date > b.date) {
+      return 1;
+    }
+    return -1;
+  });
+
+  const records: Record[] = [];
+  let currentId = 0;
+  let currentMinRange = 0;
+
+  for (const event of events) {
+    const match = event.date.match(/\d+-(.*) 00:(\d+)/);
+    let date = "";
+    let minute = 0;
+
+    if (match) {
+      date = match[1];
+      minute = parseInt(match[2], 0);
+    }
+
+    if (event.type === EventType.Start) {
+      currentId = event.id;
+    }
+
+    if (event.type === EventType.Sleep) {
+      currentMinRange = minute;
+    }
+
+    if (event.type === EventType.Wake) {
+      // Create array of minutes asleep
+      const mins = [];
+      for (let min = currentMinRange; min < minute; min++) {
+        mins.push(min);
+      }
+
+      records.push({
+        date,
+        id: currentId,
+        mins
+      });
+    }
+  }
+
+  return records;
+}
+
+// return ID x minute
+export function findMostMinutesID(records: Record[]): number {
+  const totals = new Map<number, number>();
+
+  for (const record of records) {
+    const { id } = record;
+    const total = totals.get(id) || 0;
+    totals.set(id, total + record.mins.length);
+  }
+
+  const s = Array.from(totals).sort((a, b) => {
+    return a[1] === b[1] ? 0 : a[1] < b[1] ? 1 : -1;
+  });
+
+  return s[0][0];
+}
+
+export function findMostCommonMinuteForId(id: number, records: Record[]) {
+  // Create map of minutes to occurance
+  const counts = flatten(
+    records.filter(r => r.id === id).map(r => r.mins)
+  ).reduce<{ [key: number]: number }>((counts, minute) => {
+    counts[minute] = (counts[minute] || 0) + 1;
+    return counts;
+  }, {});
+
+  // Order minutes by frequency
+  const [mostCommonMinute] = Object.keys(counts).sort(
+    (a, b) => counts[parseInt(b, 0)] - counts[parseInt(a, 0)]
+  );
+
+  return parseInt(mostCommonMinute, 0);
+}
--- day-4/src/index.ts
+++ day-4/src/index.ts
@@ -1,4 +1,9 @@
 import { readFileSync } from "fs";
+import {
+  findMostCommonMinuteForId,
+  findMostMinutesID,
+  transform
+} from "./findMostMinutes";
 
 function input() {
   return readFileSync("input", { encoding: "utf-8" })
@@ -6,3 +11,11 @@ function input() {
     .split("\n");
 }
 
+const records = transform(input());
+const id = findMostMinutesID(records);
+const minute = findMostCommonMinuteForId(id, records);
+
+console.log("ID:", id);
+console.log("Minute:", minute);
+
+console.log("Day 4 - Part 1:", id * minute);
